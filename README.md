87dffe3225f6bfff732d6662395da3e1f2d0488f
mogufm4 <moguf2k@gmail.com>
2025-02-16 14:15:21 +0800

    part1 invoke handle

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Rust Tauri App
 
 Learning Rust Tauri code 
+
+
+**开发环境：**
+
+- cargo 1.84.0 (66221abde 2024-11-19)
+
+
+
+## Part 1
+
+init project 
+
+```sh 
+# init
+cargo tauri init
+
+# debug 
+cargo tauri dev
+```
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,39 @@
+// use tauri::async_runtime::Mutex;
+use std::sync::{Mutex};
+
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
+    .manage(Mutex::new(User{
+            id: 0,
+            username: "".to_string(),
+            password: "".to_string(),
+        }))
+    .invoke_handler(tauri::generate_handler![get_user, login])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn get_user(state: tauri::State<Mutex<User>>) -> User {
+    let user =&*state.lock().unwrap();
+    user.clone()
+}
+
+#[tauri::command]
+fn login(state: tauri::State<Mutex<User>>, username: String, password: String) -> bool {
+    *state.lock().unwrap() = User{
+        username, 
+        password, 
+        id: 1
+    };
+    true
+}
+
+#[derive(serde::Serialize, Clone)]
+struct User {
+    id: u32,
+    username: String,
+    password: String
 }
